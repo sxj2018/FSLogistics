95f7acab846d81bc13117e999157cc7cf0e6a15f
CN0863DY055 <CN0863DY055@CNWX0863DY055>
2020-05-09 19:42:48 +0800

    增加vue文件

---
+++ Web/Content/vue/js/qs.js
@@ -0,0 +1,34 @@
+﻿//模拟qs.js的功能，结合axios的post请求使用
+var qs = {
+    stringify: (o, type) => {
+        if (!type) {
+            type = "application/x-www-form-urlencoded";
+        }
+
+        let param;
+
+        switch (type) {
+            case "application/x-www-form-urlencoded":
+                param = "";
+                for (prop in o) {
+                    param += "&";
+                    param += prop + "=" + o[prop];
+                }
+                param = param.replace("&", "");
+                break;
+            case "multipart/form-data":
+                param = new FormData();
+                for (prop in o) {
+                    param.append(prop, o[prop]);
+                }
+                break;
+        }
+
+        if (!param) {
+            param = o;
+        }
+
+        return param;
+    }
+}
+
